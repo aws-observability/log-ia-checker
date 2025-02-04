6da0c51774d6509a64464b75ca45ab3758ad4cc9
Kevin Lewin <lewinke@amazon.com>
2025-02-04 13:12:57 -0500

    accept region argument

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/cloudtrail"
@@ -11,15 +12,21 @@ import (
 )
 
 func main() {
-	//Built a log client
-	cfg, err := config.LoadDefaultConfig(context.TODO())
+	//Get region from os arg
+	if len(os.Args) < 2 {
+		log.Fatal("Error: Missing Region Argument")
+	}
+	region := os.Args[1]
+
+	//Built a log and trail client
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(region))
 	if err != nil {
 		log.Fatalf("unable to load SDK config, %v", err)
 	}
 	log_client := cloudwatchlogs.NewFromConfig(cfg)
 	cloudtrail_client := cloudtrail.NewFromConfig(cfg)
 
-	//get list of log groups
+	//get list of log groups and perform some initial checks on the describe output
 	logList := getLogList(log_client)
 
 	//removeliveTailEvents TODO: do more stuff in cTrail
